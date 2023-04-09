68c6a77f73a1ede6d4dff98b95a2281046fe7380
Vikas Pathak <123vikas.vp79@gmail.com>
2023-04-09 14:19:33 +0530

    wild card add in proj

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ import Todomdb from './Todomdb';
 function App() {
   return (
    <>
-   {/* <h4>Connected With Local Database</h4> */}
-    {/* <Todo/> */}
+   <h4>Connected With Local Database</h4>n
+    <Todo/>
     <br/>
     <h4>Connected With Mongo Database</h4>
     <Todomdb/>
